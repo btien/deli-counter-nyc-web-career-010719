f8b9f9f1655eb6c5e86e2cfbb2e61940b6179f9b
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-03 18:06:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(katz_deli) 
+  counter = 1
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: "
+    while counter <= katz_deli.size
+      puts "#{counter}. #{katz_deli[counter-1]}"
+      counter += 1
+    end
+  end
+end
+
+def take_a_number(katz_deli, string)
+  a = katz_deli.size
+  
+end
+
+def now_serving
+  
+end
+
