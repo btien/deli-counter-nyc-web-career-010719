befaa68eeb4f2de94a6aecde5a5c1056c781c4dc
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-03 18:13:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     a = "The line is currently: "
+    b =
     while counter <= katz_deli.size
       str = "#{counter}. #{katz_deli[counter-1]}"
       counter += 1
